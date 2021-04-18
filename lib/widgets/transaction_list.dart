b2fc67a1f40b12702a0f668b4f784220e31d5b51
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-04-18 17:57:10 +0530

    final chart update

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,65 +1,59 @@
-
 import 'package:expense_app/model/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-class TransactionList extends StatelessWidget{
+
+class TransactionList extends StatelessWidget {
   final List<Transaction> transaction;
   TransactionList(this.transaction);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 600,
-      child: transaction.isEmpty ? Column(
-        children: [
-          Text('No Transaction List',
-          style: Theme.of(context).textTheme.title,
-          ), 
-          SizedBox(height: 25,),
-          Container(
-            height: 200,
-            child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
-        ],
-      ) : ListView.builder(
-        itemBuilder: (ctx, index){
-          return Card(
-           child: Row(
-             children: [
-             Container(
-               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-               decoration: BoxDecoration(
-                 border: Border.all(color: Theme.of(context).primaryColor, width: 2, )
-               ),
-               padding: EdgeInsets.all(10),
-               child: Text('Rs.${transaction[index].amount.toStringAsFixed(2)}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Theme.of(context).primaryColor,
+      child: transaction.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No Transaction List',
+                  // ignore: deprecated_member_use
+                  style: Theme.of(context).textTheme.title,
                 ),
-               )
-             ),
-             Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Text(transaction[index].title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: FittedBox(
+                                child: Text(
+                                    'Rs.${transaction[index].amount.toStringAsFixed(2)}')))),
+                    // ignore: deprecated_member_use
+                    title: Text(
+                      transaction[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat('yyyy-MM-dd').format(transaction[index].date),
+                    ),
                   ),
-                 ),
-                 Text(DateFormat('yyyy-MM-dd').format(transaction[index].date),
-                  style: TextStyle(
-                    color: Colors.grey
-                  ),
-                 )
-               ],
-             )
-           ],
-           ),
-           );
-        }, 
-        itemCount: transaction.length,
-       ),
+                );
+              },
+              itemCount: transaction.length,
+            ),
     );
   }
 }
